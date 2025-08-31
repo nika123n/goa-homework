36cd21eb83a9f687ca20815a68f9c2fb633a46ea
nika123n <nikoloznikoloz234@gmail.com>
2025-08-31 18:27:33 +0400

    commit

---
+++ day 58/classwork/.py
@@ -0,0 +1,7 @@
+# 1) შექმენით ფუნქცია(def-ის გამოყენებით), რომელიც იქნება split ფუნქციის კლონი, ანუ შეასრულებს მის ფუნქციას(split-ის გამოყენების გარეშე)
+
+def somesing_like_split(words):
+    for i in words:
+        return i
+    
+print(somesing_like_split(["my name is nika"]))
